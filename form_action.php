d1ff27008d8fd129e44b489b7f12e2475a5de1f1
Thiago Sudré Cezarino <thiagoty@gmail.com>
2020-09-02 16:34:01 -0300

    [FIX] Correção no script que salva o arquivo anexado no formulario

---
+++ form_action.php
@@ -0,0 +1,21 @@
+<?php
+
+// obter informações do arquivo do fomulario
+$nome_arquivo = basename($_FILES['arquivo']['name']);
+$extensão_arquivo = substr($nome_arquivo, strpos($nome_arquivo, '.')+1);
+$tamanho_arquivo = $_FILES['arquivo']['size']/1024;
+
+// validar a extenção do arquivo
+if ($extensão_arquivo != 'pdf') {
+	// erro
+}
+
+// copiar o arquivo para pasta temporaria
+$caminho_tmp = __DIR__ . '/tmp/';
+$caminho_arquivo_tmp = $_FILES['arquivo']['tmp_name'];
+$caminho_arquivo_uploaded_tmp = $caminho_tmp . $nome_arquivo;
+
+// var_dump($_FILES['arquivo']['tmp_name']);
+if (!copy($caminho_arquivo_tmp, $caminho_arquivo_uploaded_tmp)) {
+	// erro
+}
--- formulario.php
+++ formulario.php
@@ -1,10 +1,10 @@
-    <!DOCTYPE html>
+	    <!DOCTYPE html>
     <html lang="pt-br">
         <head>
             <meta charset="utf-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>Formulário PHP com anexo</title>
+            <title>Formulário anexo de arquivo</title>
         
             <!-- Layout -->
             <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
@@ -18,46 +18,37 @@
             <div class="container">
             <br>
             
-            <form class="form-horizontal" method="POST" action="envia.php" enctype="multipart/form-data">
+            <form class="form-horizontal" name="email_form" method="POST" action="form_action.php" enctype="multipart/form-data">
                 <fieldset>
-                
-                    <!-- Título do formulário -->
                     <legend>Formulário para envio do arquivo PDF</legend>
-                    
-                    <!-- Campo: Nome -->
                     <div class="form-group">
                         <label class="col-md-4 control-label" for="nome">Nome</label>  
                         <div class="col-md-4">
                             <input id="nome" name="nome" placeholder="Informe seu nome" class="form-control input-md" required="" type="text">
                         </div>
                     </div>
-                    
-                    <!-- Campo: Email -->
                     <div class="form-group">
                         <label class="col-md-4 control-label" for="nome">Email</label>  
                         <div class="col-md-4">
                             <input id="replyto" name="replyto" placeholder="Informe seu email" class="form-control input-md" required="" type="text">
                         </div>
                     </div>
-                    
-                    <!-- Campo: anexo --> 
                     <div class="form-group">
                         <label class="col-md-4 control-label" for="arquivo">Anexo</label>
                         <div class="col-md-4">
                         <input id="arquivo" name="arquivo" class="input-file" type="file">
-                            <span class="help-block">2MB por mensagem</span>
+                            <!-- <span class="help-block">2MB por mensagem</span> -->
                         </div>
                     </div> 
                     
-                    <!-- Botão Enviar -->
                     <div class="form-group">
-                    <label class="col-md-4 control-label" for="submit"></label>
-                    <div class="col-md-4">
-                    <button type="submit" class="btn btn-inverse">Enviar</button>
-                    </div>
+                        <label class="col-md-4 control-label" for="submit"></label>
+                        <div class="col-md-4">
+                            <button type="submit" class="btn btn-inverse">Enviar</button>
+                        </div>
                     </div>
                     
-                    </fieldset>
+                </fieldset>
             </form>
             
             </div>
